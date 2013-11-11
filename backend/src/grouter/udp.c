a8437f4a36e76bfd08d1f33b670d7d4f698156a1
jwiener88 <jason.wiener@mail.mcgill.ca>
2013-11-11 12:16:33 -0500

    created UDP.c API

--- backend/src/grouter/udp.c
+++ backend/src/grouter/udp.c
@@ -1,1 +1,16 @@
 
+
+
+//Creates a socket item, and returns a PCB integer (array index?)
+int socket(int type){
+    
+}
+int bind(int sockid, int port){
+    
+}
+int sendto(int sockid, int destip, int dport, char *message, int len){
+    
+}
+int recvfrom(int sockid, int *srcip, int *sport, char **message, int len){
+    
+}
