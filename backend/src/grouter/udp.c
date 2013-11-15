bedbf17bd175fc53470812f4141f9b1baefaf151
jwiener88 <jason.wiener@mail.mcgill.ca>
2013-11-15 14:27:51 -0500

    initial socket creation

--- backend/src/grouter/udp.c
+++ backend/src/grouter/udp.c
@@ -1,9 +1,37 @@
+#include "udp.h"
 
 
-
-//Creates a socket item, and returns a PCB integer (array index?)
-int socket(int type){
+pcb_t PCBtable[PCBTABLESIZE];
+/**
+ * Creates all of the static structures required for UDP. 
+ * 
+ */ 
+void init(){
+    int i;
+    for(i = 0; i < PCBTABLESIZE; ++i){
+        PCBtable[i].type = FREE;
+    }
     
+}
+/** 
+ * Creates a socket item, and returns an index to the PCB array. 
+ * @param 
+ */
+int socket(int type){
+    int i;
+    if (type == 1){
+        for (i = 0; i < PCBTABLESIZE; ++i){
+            if (PCBtable[i].type == FREE){
+                return i;
+            }
+        }
+      
+    }
+    else{
+        printf("IN UDP: SOCKET REQUESTED WITH UNRECOGNIZED TYPE\n");
+    }
+    
+    return -1;
 }
 int bind(int sockid, int port){
     
